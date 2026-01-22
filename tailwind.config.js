c586284b3ef7f3c5b8a783c9c2ea9b0faa73722d
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-01-22 11:19:11 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // 이 설정이 있어야 WordGuessGame.js 안의 indigo-500 같은 클래스를 인식합니다.
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
