a61c19f7e55cc095e6df7a6c97ebe0e6f729968e
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-02-08 17:19:32 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // 이 경로가 정확해야 스타일이 생성됩니다.
+    "./src/**/*.{js,jsx,ts,tsx}",
     "./public/index.html"
   ],
   theme: {
